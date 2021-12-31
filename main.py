f8d67990f71a484384320ce0ba7ed8c3f9e146a0
Tom Reed <t.reed1104@gmail.com>
2021-12-31 20:31:36 +0000

    File reader function

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ configFile = "config.json"
 inputDirectory = "input/"
 outputDirectory = "output/"
 
+## Read the file
+def readFileToString(fileName):
+    parsedFile = ""
+    try:
+        fileContent = open(fileName, "r")
+        for line in fileContent:
+            parsedFile += line
+        fileContent.close()
+        return parsedFile
+    except (OSError, IOError) as error:
+        print(">>> ERROR! - An error occured:", error)
+        return parsedFile
+
 ## App entry
 if __name__ == '__main__':
     print("> Compiling Angry Assignments")
