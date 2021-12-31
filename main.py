e44893f68511a4962526a07875c31fd762248e53
Tom Reed <t.reed1104@gmail.com>
2021-12-31 20:31:08 +0000

    Output directory logic

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ outputDirectory = "output/"
 ## App entry
 if __name__ == '__main__':
     print("> Compiling Angry Assignments")
+
+    ## Create the output directory
+    try:
+        print(">> Creating the output directory")
+        os.makedirs(outputDirectory)
+    except FileExistsError:
+        pass
+
